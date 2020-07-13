1c8e704062da66e9b2f47cd01bd92ba715c69459
victorvpg83 <56050561+victorvpg83@users.noreply.github.com>
2020-07-13 13:47:45 +0200

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('AppComponent' , () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
